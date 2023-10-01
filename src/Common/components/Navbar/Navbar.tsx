455d16f62b5485b479b32f3dcb6b994011229437
Anton <antoncodes@outlook.com>
2023-10-01 20:09:39 +0300

    init app bar

--- src/Common/components/Navbar/Navbar.tsx
+++ src/Common/components/Navbar/Navbar.tsx
@@ -1,3 +1,27 @@
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
 export default function CommonNavbar() {
-  return <div>common navbar</div>;
+  return (
+    <AppBar position="sticky" elevation={0} color="default">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          PayUI
+        </Typography>
+        <IconButton size="large" color="inherit">
+          <AccountCircle />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
 }
