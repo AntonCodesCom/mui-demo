abc13620944b6559b39e40cc35eb5592a14034da
Anton <antoncodes@outlook.com>
2023-10-01 19:50:14 +0300

    CommonNavbar init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import CommonMain from 'Common/components/Main';
+import CommonNavbar from 'Common/components/Navbar';
 
 export default function App() {
   return (
     <>
+      <CommonNavbar />
       <CommonMain />
     </>
   );
---
+++ src/Common/components/Navbar/Navbar.tsx
@@ -0,0 +1,3 @@
+export default function CommonNavbar() {
+  return <div>common navbar</div>;
+}
