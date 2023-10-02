e5a43ea59f8f901650d22b79b54d2b268605d441
Anton <antoncodes@outlook.com>
2023-10-02 14:03:34 +0300

    init content header

--- src/Common/components/Main/Main.tsx
+++ src/Common/components/Main/Main.tsx
@@ -1,9 +1,21 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Stack, Typography } from '@mui/material';
 
 export default function CommonMain() {
   return (
-    <Box>
-      <Typography>common main</Typography>
+    <Box p={2}>
+      <Stack direction="row">
+        <Typography variant="h5" component="h1">
+          Payments overview
+        </Typography>
+        <Box flex={1} />
+        <Button variant="outlined" color="inherit">
+          Export
+        </Button>
+        <Box mr={1.5} />
+        <Button variant="contained" color="primary">
+          Payment link
+        </Button>
+      </Stack>
     </Box>
   );
 }
