81e0e3367e46644e5b319754c0cc1d78574e0b29
Anton <antoncodes@outlook.com>
2023-10-09 18:33:13 +0300

    displaying more payment data

--- src/Payment/components/Table/Table.tsx
+++ src/Payment/components/Table/Table.tsx
@@ -37,116 +37,37 @@ export default function PaymentTable() {
         <TableHead>
           <TableRow>
             <TableCell></TableCell>
-            <TableCell>Payment ID</TableCell>
-            <TableCell>Status</TableCell>
             <TableCell>Amount</TableCell>
+            <TableCell>Status</TableCell>
             <TableCell>P. Method</TableCell>
             <TableCell>Creation date</TableCell>
+            <TableCell>Payment ID</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {paymentItems.map((x, i) => (
-            <TableRow key={i}>
-              <TableCell sx={{ p: 0.5 }}>
-                <Checkbox />
-              </TableCell>
-              <TableCell>{x.id}</TableCell>
-              <TableCell>
-                <PaymentStatusChip status={x.status} />
-              </TableCell>
-              <TableCell sx={{ whiteSpace: 'nowrap' }}>
-                <Typography variant="body2" display="inline" fontWeight={700}>
-                  {formatAmount(x.amount)}
-                </Typography>
-                &nbsp;
-                <Typography variant="body2" display="inline" fontWeight={300}>
-                  USD
-                </Typography>
-              </TableCell>
-              <TableCell sx={{ whiteSpace: 'nowrap' }}>•••• 4242</TableCell>
-              <TableCell>{x.createdAt.toDateString()}</TableCell>
-            </TableRow>
-          ))}
-          <TableRow>
-            <TableCell sx={{ p: 0.5 }}>
-              <Checkbox />
-            </TableCell>
-            <TableCell>06c1774-7f3d-46ad...90a8</TableCell>
-            <TableCell>
-              <Chip
-                size="small"
-                color="error"
-                label="Declined"
-                icon={<RemoveCircle />}
-              />
-            </TableCell>
-            <TableCell sx={{ whiteSpace: 'nowrap' }}>$19,623.00 USD</TableCell>
-            <TableCell sx={{ whiteSpace: 'nowrap' }}>•••• 4242</TableCell>
-            <TableCell>Oct 2, 2023, 14:30:59</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell sx={{ p: 0.5 }}>
-              <Checkbox />
-            </TableCell>
-            <TableCell>06c1774-7f3d-46ad...90a8</TableCell>
-            <TableCell>
-              <Chip
-                size="small"
-                color="success"
-                label="Succeeded"
-                icon={<CheckCircle />}
-              />
-            </TableCell>
-            <TableCell sx={{ whiteSpace: 'nowrap' }}>$19,623.00 USD</TableCell>
-            <TableCell sx={{ whiteSpace: 'nowrap' }}>•••• 4242</TableCell>
-            <TableCell>Oct 2, 2023, 14:30:59</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell sx={{ p: 0.5 }}>
-              <Checkbox />
-            </TableCell>
-            <TableCell>06c1774-7f3d-46ad...90a8</TableCell>
-            <TableCell>
-              <Chip
-                size="small"
-                color="warning"
-                label="Pending"
-                icon={<WatchLater />}
-              />
-            </TableCell>
-            <TableCell sx={{ whiteSpace: 'nowrap' }}>$19,623.00 USD</TableCell>
-            <TableCell sx={{ whiteSpace: 'nowrap' }}>•••• 4242</TableCell>
-            <TableCell>Oct 2, 2023, 14:30:59</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell sx={{ p: 0.5 }}>
-              <Checkbox />
-            </TableCell>
-            <TableCell>06c1774-7f3d-46ad...90a8</TableCell>
-            <TableCell>
-              <Chip
-                size="small"
-                color={undefined}
-                label="Refunded"
-                icon={<ChangeCircle />}
-              />
-            </TableCell>
-            <TableCell sx={{ whiteSpace: 'nowrap' }}>$19,623.00 USD</TableCell>
-            <TableCell sx={{ whiteSpace: 'nowrap' }}>•••• 4242</TableCell>
-            <TableCell>Oct 2, 2023, 14:30:59</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell sx={{ p: 0.5 }}>
-              <Checkbox />
-            </TableCell>
-            <TableCell>06c1774-7f3d-46ad...90a8</TableCell>
-            <TableCell>
-              <Chip size="small" color="info" label="Created" icon={<Info />} />
-            </TableCell>
-            <TableCell sx={{ whiteSpace: 'nowrap' }}>$19,623.00 USD</TableCell>
-            <TableCell sx={{ whiteSpace: 'nowrap' }}>•••• 4242</TableCell>
-            <TableCell>Oct 2, 2023, 14:30:59</TableCell>
-          </TableRow>
+          {paymentItems
+            .sort((a, b) => (a.createdAt < b.createdAt ? 1 : -1))
+            .map((x, i) => (
+              <TableRow key={i}>
+                <TableCell sx={{ p: 0.5 }}>
+                  <Checkbox />
+                </TableCell>
+                <TableCell sx={{ whiteSpace: 'nowrap' }}>
+                  <Typography variant="body2" display="inline" fontWeight={700}>
+                    {formatAmount(x.amount)}
+                  </Typography>{' '}
+                  <Typography variant="body2" display="inline" fontWeight={300}>
+                    USD
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <PaymentStatusChip status={x.status} />
+                </TableCell>
+                <TableCell sx={{ whiteSpace: 'nowrap' }}>•••• 4242</TableCell>
+                <TableCell>{x.createdAt.toLocaleString('en-US')}</TableCell>
+                <TableCell>{x.id}</TableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
     </TableContainer>
--- src/Payment/data/items.ts
+++ src/Payment/data/items.ts
@@ -3,10 +3,34 @@ import PaymentStatus from 'Payment/types/Status';
 
 const paymentItems: PaymentItem[] = [
   {
-    id: '06c1774-7f3d-46ad-90a8',
+    id: 'f3ace644-e658-42c3-a23e-611f9cbc4394',
     status: PaymentStatus.SUCCEEDED,
     amount: 19623,
     createdAt: new Date('2023-10-09T14:58:30Z'),
+  },
+  {
+    id: 'd54f8608-482d-4ca5-81a2-fab84eea0aa5',
+    status: PaymentStatus.PENDING,
+    amount: 6623.53,
+    createdAt: new Date('2023-10-09T15:23:31Z'),
+  },
+  {
+    id: '19e26e93-97b8-4a26-8e68-fc77c99f7a20',
+    status: PaymentStatus.DECLINED,
+    amount: 1623.53,
+    createdAt: new Date('2023-10-09T15:24:33Z'),
+  },
+  {
+    id: '3cf9286e-549e-4ba7-9f2d-7efa6b3cb2b7',
+    status: PaymentStatus.REFUNDED,
+    amount: 19623,
+    createdAt: new Date('2023-10-09T15:25:36Z'),
+  },
+  {
+    id: '8a9dfaff-8266-488c-9322-bcdfaca70416',
+    status: PaymentStatus.CREATED,
+    amount: 130,
+    createdAt: new Date('2023-10-09T15:27:40Z'),
   },
 ];
 
